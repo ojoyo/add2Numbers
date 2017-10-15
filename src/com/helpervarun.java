4cef65e85441a544083004ebfe22f03917288729
datreon <timepass.datreon@gmail.com>
2017-10-15 16:46:12 +0530

    initial commit by varun

---
+++ src/com/helpervarun.java
@@ -0,0 +1,31 @@
+package com;
+
+public class helpervarun {
+
+    int a=20,b=50,c;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void add(){
+
+        c= a+b;
+
+        System.out.println("sum of two no is" +c);
+
+    }
+
+}
