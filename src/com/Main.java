8cc87ac8bf9fff7f75b048c44beab10a3e7c3f3c
joy <jalmeenkaursethi@gmail.com>
2017-10-15 16:31:22 +0530

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//TODO : Helper Class
+    //TODO : Varun - Do it by command line arguments
+    //TODO : Puneet - Do it by method overloading
+    //TODO : Yogesh - Do it by making simple add method
+
+
+    }
+}
